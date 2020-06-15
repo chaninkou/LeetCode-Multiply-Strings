56fbba84c307cc8600f91ee2ff4dd8480a47858d
chaninkou <michaelkoulove@yahoo.com>
2020-06-14 21:07:33 -0700

    Redid problem on stringbuilder by working from last element to first.

--- LeetCode-Multiply-Strings/src/multiply/MultiplyStringSolution.java
+++ LeetCode-Multiply-Strings/src/multiply/MultiplyStringSolution.java
@@ -7,33 +7,34 @@ public class MultiplyStringSolution {
 		
 		int[] position = new int[a + b];
 		
+		// From right to left 
 		for(int i = a - 1; i >= 0; i--){
 			for(int j = b - 1; j >= 0; j--){
-				
 				int multiply = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
 				
+				// The index positions to store 
 				int p1 = i + j, p2 = i + j + 1;
-				
-//				System.out.println(multiply);
-//				System.out.println(position[p2]);
 				
 				int sum = multiply + position[p2];
 				
+				// To get the carry
 				position[p1] += sum / 10;
-//				System.out.println(position[p1]);
-				
+
+				// To get the last digit of the multiply
 				position[p2] = sum % 10;
-//				System.out.println(position[p2]);
+
 			}
 		}
 		
 		StringBuilder sb = new StringBuilder();
 		
 		for(int p : position){
+			// Don't add any zero at the front
 			if(!(sb.length() == 0 && p == 0)){
 				sb.append(p);
 			}
 		}
+		
 		if(sb.length() == 0){
 			return "0";
 		}else{
